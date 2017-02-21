5b68dda0f458edcf7b9b9486b7cca68992c25024
akshaysonavane <akshaysonavane0@gmail.com>
2017-02-21 08:53:19 +0530

    Add files via upload

---
+++ assets/faqanime/faqanime_edge.js
@@ -0,0 +1,7 @@
+
+(function(compId){var _=null,y=true,n=false,x1='6.0.0',x3='6.0.0.400',x2='5.0.0',x5='rgba(9,113,178,1.00)',i='none';var g4='Untitled-1.mp4';var im='images/',aud='media/',vid='media/',js='js/',fonts={},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'Untitled-1',t:'video',tag:'video',r:['0px','0px','350','342','auto','auto'],ap:'autoplay',lp:'loop',sr:[vid+g4],pr:'auto'}],style:{'${Stage}':{isStage:true,r:[undefined,undefined,'100%','100%'],overflow:'hidden',f:[x5]}}},tt:{d:0,a:y,data:[]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-112820758");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){})("stage");
+//Edge symbol end:'stage'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-112820758");
